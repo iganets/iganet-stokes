ffa9e8d0a629f7c66c36167fc47b787a55267b37
Matthias Moeller <m.moller@tudelft.nl>
2021-11-26 15:21:35 +0100

    Evaluation of B-spline functions working

--- src/iganet.cxx
+++ src/iganet.cxx
@@ -5,8 +5,16 @@ int main()
 {
   torch::manual_seed(1);
   
-  iganet::IgANet<double, 2, 2> net( {50,30,70}, // Number of neurons per layers
-                                    {5,7}       // B-spline knots
-                                    );
-  net.plot();
+  iganet::IgANet<double, 2,2> net( {50,30,70}, // Number of neurons per layers
+                                   {6,6}       // B-spline knots
+                                   );
+  
+  //net.sol().transform( [](const std::array<double,1> X){ return std::array<double,1>{ sin(3.141*X[0]) }; } );
+  net.sol().transform( [](const std::array<double,2> X){ return std::array<double,1>{ sin(3.141*X[0])*sin(3.141*X[1]) }; } );
+  //net.sol().transform( [](const std::array<double,3> X){ return std::array<double,1>{ sin(3.141*X[0])*sin(3.141*X[1])*sin(3.141*X[2]) }; } );
+  //net.sol().transform( [](const std::array<double,4> X){ return std::array<double,1>{ sin(3.141*X[0])*sin(3.141*X[1])*sin(3.141*X[2])*sin(3.141*X[3]) }; } );
+  
+  std::cout << net.sol().eval( torch::full({2}, 0.5) ) << std::endl;
+
+  //net.plot();
 }
