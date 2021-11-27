bce4697da03bc229ee8cefa4d390cd0f8fea88e6
Matthias Moeller <m.moller@tudelft.nl>
2021-11-27 16:41:09 +0100

    Fixed bugs in evaluation of B-splines

--- src/iganet.cxx
+++ src/iganet.cxx
@@ -6,16 +6,18 @@ int main()
   torch::manual_seed(1);
   iganet::core<double> core;
   
-  iganet::IgANet<double, 2,2> net( {50,30,70}, // Number of neurons per layers
-                                   {6,6}       // B-spline knots
-                                   );
+  iganet::IgANet<double,5,5,5,5> net( {50,30,70}, // Number of neurons per layers
+                                      {6,6,6,6}       // Number of B-spline coefficients
+                                  );
+
+  std::cout << net << std::endl;
   
-  //net.sol().transform( [](const std::array<double,1> X){ return std::array<double,1>{ sin(3.141*X[0]) }; } );
-  net.sol().transform( [](const std::array<double,2> X){ return std::array<double,1>{ sin(3.141*X[0])*sin(3.141*X[1]) }; } );
-  //net.sol().transform( [](const std::array<double,3> X){ return std::array<double,1>{ sin(3.141*X[0])*sin(3.141*X[1])*sin(3.141*X[2]) }; } );
-  //net.sol().transform( [](const std::array<double,4> X){ return std::array<double,1>{ sin(3.141*X[0])*sin(3.141*X[1])*sin(3.141*X[2])*sin(3.141*X[3]) }; } );
-  
-  std::cout << net.sol().eval( torch::full({2}, 0.5, core.options_) ) << std::endl;
+  //net.sol().transform( [](const std::array<double,1> X){ return std::array<double,1>{ X[0] /*sin(M_PI*X[0])*/ }; } );
+  //net.sol().transform( [](const std::array<double,2> X){ return std::array<double,1>{ sin(M_PI*X[0])*sin(M_PI*X[1]) }; } );
+  //net.sol().transform( [](const std::array<double,3> X){ return std::array<double,1>{ sin(M_PI*X[0])*sin(M_PI*X[1])*sin(M_PI*X[2]) }; } );
+  net.sol().transform( [](const std::array<double,4> X){ return std::array<double,1>{ sin(M_PI*X[0])*sin(M_PI*X[1])*sin(M_PI*X[2])*sin(M_PI*X[3]) }; } );
+
+  std::cout << net.sol().eval( torch::stack({torch::full({1}, 0.5), torch::full({1}, 1.0), torch::full({1}, 1.0), torch::full({1}, 1.0)}).flatten() ) << std::endl;
   
   //net.plot();
 }
