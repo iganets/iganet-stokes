08e2bd6ba6f9be49fee1fc6429544bccb3b0abf0
Matthias Möller <m.moller@tudelft.nl>
2022-02-04 11:41:44 +0100

    Extended examples to non-uniform B-Splines

--- src/iganet.cxx
+++ src/iganet.cxx
@@ -7,40 +7,55 @@ int main()
   torch::manual_seed(1);
 
   {
-    iganet::IgANet<real_t,5> net( {50,30,70}, // Number of neurons per layers
-                                  {6}         // Number of B-spline coefficients
-                                  );
+    iganet::IgANet<real_t,iganet::UniformBSpline,5> net({50,30,70}, // Number of neurons per layers
+                                                        {6});       // Number of B-spline coefficients
     std::cout << net << std::endl;
-    net.sol().transform( [](const std::array<real_t,1> X){ return std::array<real_t,1>{ X[0] /*sin(M_PI*X[0])*/ }; } );
+    net.sol().transform( [](const std::array<real_t,1> X){ return std::array<real_t,1>{ X[0]*sin(M_PI*X[0]) }; } );
     std::cout << net.sol().eval( torch::stack({torch::full({1}, 0.5)}).flatten() ) << std::endl;
   }
 
   {
-    iganet::IgANet<real_t,5,5> net( {50,30,70}, // Number of neurons per layers
-                                    {6,6}       // Number of B-spline coefficients
-                                    );
+    iganet::IgANet<real_t,iganet::UniformBSpline,5,5> net({50,30,70}, // Number of neurons per layers
+                                                          {6,6});     // Number of B-spline coefficients
     std::cout << net << std::endl;
     net.sol().transform( [](const std::array<real_t,2> X){ return std::array<real_t,1>{ sin(M_PI*X[0])*sin(M_PI*X[1]) }; } );
     std::cout << net.sol().eval( torch::stack({torch::full({1}, 0.5), torch::full({1}, 0.5)}).flatten() ) << std::endl;
+
+    //net.plot();
   }
 
   {
-    iganet::IgANet<real_t,5,5,5> net( {50,30,70}, // Number of neurons per layers
-                                      {6,6,6}     // Number of B-spline coefficients
-                                      );
+    iganet::IgANet<real_t,iganet::UniformBSpline,5,5,5> net({50,30,70}, // Number of neurons per layers
+                                                            {6,6,6});   // Number of B-spline coefficients
     std::cout << net << std::endl;
     net.sol().transform( [](const std::array<real_t,3> X){ return std::array<real_t,1>{ sin(M_PI*X[0])*sin(M_PI*X[1])*sin(M_PI*X[2]) }; } );
     std::cout << net.sol().eval( torch::stack({torch::full({1}, 0.5), torch::full({1}, 0.5), torch::full({1}, 0.5)}).flatten() ) << std::endl;
   }
 
   {
-    iganet::IgANet<real_t,5,5,5,5> net( {50,30,70}, // Number of neurons per layers
-                                        {6,6,6,6}   // Number of B-spline coefficients
-                                        );
+    iganet::IgANet<real_t,iganet::UniformBSpline,5,5,5,5> net({50,30,70}, // Number of neurons per layers
+                                                              {6,6,6,6}); // Number of B-spline coefficients
     std::cout << net << std::endl;
     net.sol().transform( [](const std::array<real_t,4> X){ return std::array<real_t,1>{ sin(M_PI*X[0])*sin(M_PI*X[1])*sin(M_PI*X[2])*sin(M_PI*X[3]) }; } );
     std::cout << net.sol().eval( torch::stack({torch::full({1}, 0.5), torch::full({1}, 0.5), torch::full({1}, 0.5), torch::full({1}, 0.5)}).flatten() ) << std::endl;
   }
-  
-  //net.plot();
+
+
+  {
+    std::array<std::vector<real_t>,2> kv =
+      {
+        {
+          {0,0,0,0.2, 0.3, 1,1,1},
+          {0,0,0,0.7,1,1,1}
+        }
+      };
+    
+    iganet::IgANet<real_t,iganet::NonUniformBSpline,2,2> net({50,30,70}, // Number of neurons per layers
+                                                          kv);        // Knot vectors
+    std::cout << net << std::endl;
+    net.sol().transform( [](const std::array<real_t,2> X){ return std::array<real_t,1>{ sin(M_PI*X[0])*sin(M_PI*X[1]) }; } );
+    std::cout << net.sol().eval( torch::stack({torch::full({1}, 0.5), torch::full({1}, 0.5)}).flatten() ) << std::endl;
+
+    net.plot();
+  }  
 }
