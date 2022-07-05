c53c238bb4923ba1ca2c099ed5c50680142396ea
Matthias Möller <m.moller@tudelft.nl>
2022-07-05 14:53:31 +0200

    Update application

--- src/iganet.cxx
+++ src/iganet.cxx
@@ -17,28 +17,58 @@
 
 int main()
 {
-  using real_t      = double;
+  using real_t      = float;
   using optimizer_t = torch::optim::Adam;
   
   iganet::init();
   iganet::verbose(std::cout);
+  
+  {
+    iganet::IgANet<real_t, optimizer_t,
+                   iganet::UniformBSpline,
+                   1> net({100,100}, // Number of neurons per layers
+                          {
+                            {iganet::activation::relu},
+                            {iganet::activation::relu},
+                            {iganet::activation::none}
+                          },         // Activation functions
+                          {11});     // Number of B-spline coefficients
 
+    // Set rhs to x
+    net.rhs().transform( [](const std::array<real_t,1> X){ return std::array<real_t,1>{ static_cast<real_t>( X[0] ) }; } );
+
+    // Set left boundary value to 0
+    net.bdr().coeffs()[0].accessor<real_t ,1>()[0] = 0;
+    net.bdr().coeffs()[1].accessor<real_t,1>()[0] = 1;
+
+    net.train();
+  }
+
+  return 0;
+  
   {
     iganet::IgANet<real_t, optimizer_t,
                    iganet::UniformBSpline,
                    5> net({50,30,70}, // Number of neurons per layers
+                          {
+                            {iganet::activation::relu},
+                            {iganet::activation::relu},
+                            {iganet::activation::relu},
+                            {iganet::activation::none}
+                          },          // Activation functions
                           {6});       // Number of B-spline coefficients
     std::cout << "Saved IgaNet1\n";
     std::cout << net << std::endl;
-    net.sol().transform( [](const std::array<real_t,1> X){ return std::array<real_t,1>{ X[0]*sin(M_PI*X[0]) }; } );
+    net.sol().transform( [](const std::array<real_t,1> X){ return std::array<real_t,1>{ static_cast<real_t>( X[0]*sin(M_PI*X[0]) ) }; } );
     std::cout << net.sol().eval( torch::stack({torch::full({1}, 0.5)}).flatten() ) << std::endl;
 
     net.save("iganet1.pt");
 
-    // iganet::IgANet<real_t, optimizer_t, iganet::UniformBSpline,
-    //                5> net1;
+    iganet::IgANet<real_t, optimizer_t,
+                   iganet::UniformBSpline,
+                   5> net1;
 
-    // net1.load("iganet1.pt");
+    net1.load("iganet1.pt");
     // std::cout << "Loaded IgaNet1\n";
     // std::cout << net1 << std::endl;
 
@@ -48,10 +78,16 @@ int main()
   {
     iganet::IgANet<real_t, optimizer_t, iganet::UniformBSpline,
                    2, 2> net({50,30,70}, // Number of neurons per layers
+                             {
+                               {iganet::activation::relu},
+                               {iganet::activation::relu},
+                               {iganet::activation::relu},
+                               {iganet::activation::none}
+                             },          // Activation functions
                              {3,6});     // Number of B-spline coefficients
     std::cout << "Saved IgaNet2\n";
     std::cout << net << std::endl;
-    net.sol().transform( [](const std::array<real_t,2> X){ return std::array<real_t,1>{ sin(M_PI*X[0])*sin(M_PI*X[1]) }; } );
+    net.sol().transform( [](const std::array<real_t,2> X){ return std::array<real_t,1>{ static_cast<real_t>( sin(M_PI*X[0])*sin(M_PI*X[1]) ) }; } );
     std::cout << net.sol().eval( torch::stack({torch::full({1}, 0.5), torch::full({1}, 0.5)}).flatten() ) << std::endl;
 
     net.save("iganet2.pt");
@@ -60,10 +96,16 @@ int main()
   {
     iganet::IgANet<real_t, optimizer_t, iganet::UniformBSpline,
                    5, 5, 5> net({50,30,70}, // Number of neurons per layers
+                                {
+                                  {iganet::activation::relu},
+                                  {iganet::activation::relu},
+                                  {iganet::activation::relu},
+                                  {iganet::activation::none}
+                                },          // Activation functions
                                 {6,6,6});   // Number of B-spline coefficients
     std::cout << "Saved IgaNet3\n";
     std::cout << net << std::endl;
-    net.sol().transform( [](const std::array<real_t,3> X){ return std::array<real_t,1>{ sin(M_PI*X[0])*sin(M_PI*X[1])*sin(M_PI*X[2]) }; } );
+    net.sol().transform( [](const std::array<real_t,3> X){ return std::array<real_t,1>{ static_cast<real_t>( sin(M_PI*X[0])*sin(M_PI*X[1])*sin(M_PI*X[2]) ) }; } );
     std::cout << net.sol().eval( torch::stack({torch::full({1}, 0.5), torch::full({1}, 0.5), torch::full({1}, 0.5)}).flatten() ) << std::endl;
 
     net.save("iganet3.pt");
@@ -72,10 +114,16 @@ int main()
   {
     iganet::IgANet<real_t, optimizer_t, iganet::UniformBSpline, 
                    5, 5, 5, 5> net({50,30,70}, // Number of neurons per layers
+                                   {
+                                     {iganet::activation::relu},
+                                     {iganet::activation::relu},
+                                     {iganet::activation::relu},
+                                     {iganet::activation::none}
+                                   },          // Activation functions
                                    {6,6,6,6}); // Number of B-spline coefficients
     std::cout << "Saved IgaNet4\n";
     std::cout << net << std::endl;
-    net.sol().transform( [](const std::array<real_t,4> X){ return std::array<real_t,1>{ sin(M_PI*X[0])*sin(M_PI*X[1])*sin(M_PI*X[2])*sin(M_PI*X[3]) }; } );
+    net.sol().transform( [](const std::array<real_t,4> X){ return std::array<real_t,1>{ static_cast<real_t>( sin(M_PI*X[0])*sin(M_PI*X[1])*sin(M_PI*X[2])*sin(M_PI*X[3]) ) }; } );
     std::cout << net.sol().eval( torch::stack({torch::full({1}, 0.5), torch::full({1}, 0.5), torch::full({1}, 0.5), torch::full({1}, 0.5)}).flatten() ) << std::endl;
 
     net.save("iganet4.pt");
