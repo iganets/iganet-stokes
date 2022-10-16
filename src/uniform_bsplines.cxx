3986af0f6aa4bca0363e8b66586a116a8f4dbfe1
Matthias Moller <mmoelle1@gmail.com>
2022-10-16 14:34:45 +0200

    Colorized plotting

--- src/uniform_bsplines.cxx
+++ src/uniform_bsplines.cxx
@@ -22,7 +22,7 @@ int main()
 
   {
     // Univariate uniform B-spline of degree 2 with 6 control points in R^1
-    iganet::UniformBSpline<double,1,2> bspline({6});
+    iganet::UniformBSpline<double,1,2> bspline({6}), color({6});
 
     // Print information
     std::cout << bspline << std::endl;
@@ -30,16 +30,20 @@ int main()
     // Map control points to phyiscal coordinates
     bspline.transform( [](const std::array<real_t,1> xi){ return std::array<real_t,1>{ xi[0]*xi[0] }; } );
 
+    // Map colors
+    color.transform( [](const std::array<real_t,1> xi){ return std::array<real_t,1>{ xi[0] }; } );
+    
     // Evaluate B-spline at xi=0, xi=0.5, and xi=1
     std::cout << bspline.eval( iganet::to_tensor({0.0}) ) << std::endl
               << bspline.eval( iganet::to_tensor({0.5}) ) << std::endl
               << bspline.eval( iganet::to_tensor({1.0}) ) << std::endl;
 
     // Evaluate B-spline at xi=0, xi=0.5, and xi=1
-    std::cout << bspline.eval_( iganet::to_tensor({0.0, 0.5, 1.0}) ) << std::endl;
+    std::cout << bspline.eval_( iganet::to_tensorArray({0.0, 0.5, 1.0}) ) << std::endl;
     
     // Plot B-spline
     bspline.plot(50);
+    bspline.plot(color, 50);
 
     // Export B-spline to XML
     std::cout << bspline.to_xml() << std::endl;
@@ -48,6 +52,7 @@ int main()
   {
     // Univariate uniform B-spline of degree 2 with 6 control points in R^2
     iganet::UniformBSpline<double,2,2> bspline({6});
+    iganet::UniformBSpline<double,1,2> color({6});
 
     // Print information
     std::cout << bspline << std::endl;
@@ -56,16 +61,20 @@ int main()
     bspline.transform( [](const std::array<real_t,1> xi){ return std::array<real_t,2>{ xi[0]*xi[0],
                                                                                        sin(M_PI*xi[0]) }; } );
 
+    // Map colors
+    color.transform( [](const std::array<real_t,1> xi){ return std::array<real_t,1>{ xi[0] }; } );
+    
     // Evaluate B-spline at xi=0, xi=0.5, and xi=1
     std::cout << bspline.eval( iganet::to_tensor({0.0}) ) << std::endl
               << bspline.eval( iganet::to_tensor({0.5}) ) << std::endl
               << bspline.eval( iganet::to_tensor({1.0}) ) << std::endl;
 
     // Evaluate B-spline at xi=0, xi=0.5, and xi=1
-    std::cout << bspline.eval_( iganet::to_tensor({0.0, 0.5, 1.0}) ) << std::endl;
+    std::cout << bspline.eval_( iganet::to_tensorArray({0.0, 0.5, 1.0}) ) << std::endl;
     
     // Plot B-spline
     bspline.plot(50);
+    bspline.plot(color, 50);
 
     // Export B-spline to XML
     std::cout << bspline.to_xml() << std::endl;
@@ -74,6 +83,7 @@ int main()
   {
     // Univariate uniform B-spline of degree 2 with 6 control points in R^3
     iganet::UniformBSpline<double,3,2> bspline({6});
+    iganet::UniformBSpline<double,1,2> color({6});
 
     // Print information
     std::cout << bspline << std::endl;
@@ -83,16 +93,20 @@ int main()
                                                                                        sin(M_PI*xi[0]),
                                                                                        xi[0]          }; } );
 
+    // Map colors
+    color.transform( [](const std::array<real_t,1> xi){ return std::array<real_t,1>{ xi[0] }; } );
+    
     // Evaluate B-spline at xi=0, xi=0.5, and xi=1
     std::cout << bspline.eval( iganet::to_tensor({0.0}) ) << std::endl
               << bspline.eval( iganet::to_tensor({0.5}) ) << std::endl
               << bspline.eval( iganet::to_tensor({1.0}) ) << std::endl;
 
     // Evaluate B-spline at xi=0, xi=0.5, and xi=1
-    std::cout << bspline.eval_( iganet::to_tensor({0.0, 0.5, 1.0}) ) << std::endl;
+    std::cout << bspline.eval_( iganet::to_tensorArray({0.0, 0.5, 1.0}) ) << std::endl;
     
     // Plot B-spline
     bspline.plot(50);
+    bspline.plot(color, 50);
 
     // Export B-spline to XML
     std::cout << bspline.to_xml() << std::endl;
@@ -102,6 +116,7 @@ int main()
     // Bivariate uniform B-spline of degree 3 in xi-direction and 4
     // in eta-direction with 5 x 6 control points in R^2
     iganet::UniformBSpline<double,2,3,4> bspline({5,6});
+    iganet::UniformBSpline<double,1,3,4> color({5,6});
 
     // Print information
     std::cout << bspline << std::endl;
@@ -110,16 +125,20 @@ int main()
     bspline.transform( [](const std::array<real_t,2> xi){ return std::array<real_t,2>{(xi[0]+1)*cos(M_PI*xi[1]),
                                                                                       (xi[0]+1)*sin(M_PI*xi[1])}; } );
 
+    // Map colors
+    color.transform( [](const std::array<real_t,2> xi){ return std::array<real_t,1>{ xi[0]*xi[1] }; } );
+    
     // Evaluate B-spline at (xi=0,eta=0), (xi=0.5,eta=0.5), and (xi=1,eta=0.5)
     std::cout << bspline.eval( iganet::to_tensor({0.0, 0.0}) ) << std::endl
               << bspline.eval( iganet::to_tensor({0.5, 0.5}) ) << std::endl
               << bspline.eval( iganet::to_tensor({1.0, 0.5}) ) << std::endl;
 
     // Evaluate B-spline at xi=0, xi=0.5, and xi=1
-    std::cout << bspline.eval_( iganet::to_tensor({0.0, 0.5, 1.0, 0.0, 0.5, 0.5}, {2,3}) ) << std::endl;
+    std::cout << bspline.eval_( iganet::to_tensorArray({0.0, 0.5, 1.0}, {0.0, 0.5, 0.5}) ) << std::endl;
     
     // Plot B-spline
-    bspline.plot(50,50);
+    bspline.plot(50, 50);
+    bspline.plot(color, 50, 50);
 
     // Export B-spline to XML
     std::cout << bspline.to_xml() << std::endl;
@@ -129,6 +148,7 @@ int main()
     // Bivariate uniform B-spline of degree 3 in xi-direction and 4
     // in eta-direction with 5 x 6 control points in R^3
     iganet::UniformBSpline<double,3,3,4> bspline({5,6});
+    iganet::UniformBSpline<double,1,3,4> color({5,6});
 
     // Print information
     std::cout << bspline << std::endl;
@@ -138,13 +158,17 @@ int main()
                                                                                       (xi[0]+1)*sin(M_PI*xi[1]),
                                                                                        xi[0] }; } );
 
+    // Map colors
+    color.transform( [](const std::array<real_t,2> xi){ return std::array<real_t,1>{ xi[0]*xi[1] }; } );
+    
     // Evaluate B-spline at (xi=0,eta=0), (xi=0.5,eta=0.5), and (xi=1,eta=0.5)
     std::cout << bspline.eval( iganet::to_tensor({0.0, 0.0}) ) << std::endl
               << bspline.eval( iganet::to_tensor({0.5, 0.5}) ) << std::endl
               << bspline.eval( iganet::to_tensor({1.0, 0.5}) ) << std::endl;
 
     // Plot B-spline
-    bspline.plot(50,50);
+    bspline.plot(50, 50);
+    bspline.plot(color, 50, 50);
 
     // Export B-spline to XML
     std::cout << bspline.to_xml() << std::endl;
