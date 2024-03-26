192ffbd7f399590bd33a97399996380e13d93664
Matthias Moller <mmoelle1@gmail.com>
2024-03-26 17:14:08 +0100

    Updated auto-tune example

--- src/iganet_fitting_autotune.cxx
+++ src/iganet_fitting_autotune.cxx
@@ -1,11 +1,11 @@
 /**
    @file examples/iganet_fitting_autotune.cxx
 
-   @brief Demonstration of IgANet function fitting with automatix
+   @brief Demonstration of IgANet function fitting with automatic
    hyper-parameter tuning
 
    This example demonstrates how to auto-tune the hyper-parameters of
-   an IgANet for fiting a given function on a square geometry.
+   an IgANet for fitting a given function on a square geometry.
 
    @author Matthias Moller
 
@@ -54,28 +54,24 @@ public:
   ///
   /// @param[in] epoch Epoch number
   bool epoch(int64_t epoch) override {
-    iganet::Log(iganet::log::verbose)
-        << "Epoch " << std::to_string(epoch) << ": ";
-
     // In the very first epoch we need to generate the sampling points
     // for the inputs and the sampling points in the function space of
     // the variables since otherwise the respective tensors would be
     // empty. In all further epochs no updates are needed since we do
     // not change the inputs nor the variable function space.
-    if (epoch == 0) {
-      Base::inputs(epoch);
-      collPts_ = Base::variable_collPts(iganet::collPts::greville);
+    //    if (epoch == 0) {
+    collPts_ = Base::variable_collPts(iganet::collPts::greville);
 
-      knot_indices_ =
-          Base::f_.template find_knot_indices<iganet::functionspace::interior>(
-              collPts_.first);
-      coeff_indices_ =
-          Base::f_.template find_coeff_indices<iganet::functionspace::interior>(
-              knot_indices_);
+    knot_indices_ =
+        Base::f_.template find_knot_indices<iganet::functionspace::interior>(
+            collPts_.first);
+    coeff_indices_ =
+        Base::f_.template find_coeff_indices<iganet::functionspace::interior>(
+            knot_indices_);
 
-      return true;
-    } else
-      return false;
+    return true;
+    //} else
+    // return false;
   }
 
   /// @brief Computes the loss function
@@ -84,11 +80,10 @@ public:
   ///
   /// @param[in] epoch Epoch number
   torch::Tensor loss(const torch::Tensor &outputs, int64_t epoch) override {
-
     // Cast the network output (a raw tensor) into the proper
     // function-space format, i.e. B-spline objects for the interior
     // and boundary parts that can be evaluated.
-    Base::u_.from_tensor(outputs);
+    Base::u_.from_tensor(outputs.flatten());
 
     // Evaluate the loss function
     return torch::mse_loss(
@@ -99,7 +94,7 @@ public:
 
 int main() {
   iganet::init();
-  iganet::verbose(iganet::Log(iganet::log::info));
+  iganet::verbose(std::cout);
 
   nlohmann::json json;
   json["res0"] = 50;
@@ -110,20 +105,40 @@ int main() {
   using real_t = double;
 
   // Geometry: Bi-linear B-spline function space S2 (geoDim = 2, p = q = 1)
-  using geometry_t = iganet::S2<iganet::UniformBSpline<real_t, 2, 1, 1>>;
+  using geometry_t = iganet::S2<iganet::UniformBSpline<real_t, 2, 2, 2>>;
 
   // Variable: Bi-quadratic B-spline function space S2 (geoDim = 1, p = q = 2)
   using variable_t = iganet::S2<iganet::UniformBSpline<real_t, 1, 2, 2>>;
+
+  // Data set for training
+  iganet::IgADataset<> data_set;
+  data_set.add_geometryMap(geometry_t{iganet::utils::to_array(25_i64, 25_i64)},
+                           IGANET_DATA_DIR "surfaces/2d");
+
+  // Impose solution value for supervised training (not right-hand side)
+  data_set.add_referenceData(
+      variable_t{iganet::utils::to_array(30_i64, 30_i64)},
+      [](const std::array<real_t, 2> xi) {
+        return std::array<real_t, 1>{
+            static_cast<real_t>(sin(M_PI * xi[0]) * sin(M_PI * xi[1]))};
+      });
+
+  auto train_set = data_set.map(
+      torch::data::transforms::Stack<iganet::IgADataset<>::example_type>());
+  auto train_size = train_set.size().value();
+  auto train_loader =
+      torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
+          std::move(train_set), 1);
 
   for (std::vector<std::any> activation :
        {std::vector<std::any>{iganet::activation::relu},
         std::vector<std::any>{iganet::activation::sigmoid},
         std::vector<std::any>{iganet::activation::tanh}}) {
-    for (int64_t nlayers : {1, 2, 3, 4, 5, 6, 7}) {
-      for (int64_t nneurons : {20, 40, 60, 80, 100}) {
+    for (int64_t nlayers : {5, 6, 7, 8, 9}) {
+      for (int64_t nneurons : {60, 80, 100, 120, 140, 160, 180, 200}) {
 
         iganet::Log(iganet::log::info)
-            << "nlayers: " << nlayers << ", nneurons: " << nneurons
+            << "#layers: " << nlayers << ", #neurons: " << nneurons
             << std::endl;
 
         std::vector<int64_t> layers(nlayers, nneurons);
@@ -136,20 +151,13 @@ int main() {
                 layers,
                 // Activation functions
                 activations,
-                // Number of B-spline coefficients of the geometry, just [0,1] x
-                // [0,1]
-                std::tuple(iganet::utils::to_array(2_i64, 2_i64)),
+                // Number of B-spline coefficients of the geometry
+                std::tuple(iganet::utils::to_array(25_i64, 25_i64)),
                 // Number of B-spline coefficients of the variable
-                std::tuple(iganet::utils::to_array(10_i64, 10_i64)));
+                std::tuple(iganet::utils::to_array(30_i64, 30_i64)));
 
-        // Impose solution value for supervised training (not right-hand side)
-        net.f().transform([](const std::array<real_t, 2> xi) {
-          return std::array<real_t, 1>{
-              static_cast<real_t>(sin(M_PI * xi[0]) * sin(M_PI * xi[1]))};
-        });
-
-        // Set maximum number of epoches
-        net.options().max_epoch(1000);
+        // Set maximum number of epochs
+        net.options().max_epoch(500);
 
         // Set tolerance for the loss functions
         net.options().min_loss(1e-8);
@@ -158,7 +166,7 @@ int main() {
         auto t1 = std::chrono::high_resolution_clock::now();
 
         // Train network
-        net.train();
+        net.train(*train_loader);
 
         // Stop time measurement
         auto t2 = std::chrono::high_resolution_clock::now();
